5cc708526972b77d61b9f6eefadf8ac6c0681a8d
Brenins Camilo Alonso Franco <brenins.az09@gmail.com>
2023-10-21 08:27:09 -0500

    proyecto-programacion V1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import elemento1 from './components/componente-uno/ElementOne';
 
-function App() {
-  const [count, setCount] = useState(0)
+//const elemento1 = <h1>Hello world! one</h1>;
+const name = "james rodriguez";
+const element = <h1>Hello {`2+2 = ${2+2} ${name}`}</h1>
+const objExample = {
+name: 'jhon', 
+lasName: 'vargas', 
+age: 30, 
+city: 'new york'
+};
+const fullName = (
+  <div>
+    <h1>Hola este es el nombre completo {formatName(objExample)}</h1>
+    <h2>Hola este es el nombre completo {formatName(objExample)}</h2>
+  </div>
+  )
+const valorAtributo = 2
 
+function formatName(user) {
+  return user.name + ' ' + user.lasName;
+}
+
+function renderAge(user){
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>La edad es de: {user.age}</h1>
+      <h2>La edad es de: {user.age}</h2>
     </>
+  )
+}
+
+function App() {
+  //const [count, setCount] = useState(0)
+  const elemento2 = <h1 id='id-unico' tabIndex={valorAtributo} className='hola2'>Hello world! two</h1>
+
+  return (
+    <div>
+      <elemento1 />
+      {elemento2}
+      {element}
+      <p style={{
+        border: 'red solid',
+        borderRadius: '18px',
+        backgroundColor: 'yellow'
+        }}
+      >
+        el nombre completo es: {formatName(objExample)}</p>
+      {fullName}
+      {renderAge(objExample)}
+    </div>
   )
 }
 
---
+++ src/components/componente-uno/ElementOne.jsx
@@ -0,0 +1,5 @@
+//const elemento1 = <h1>Hello world! one</h1>;
+function elemento1(){
+  return  <h1>Hello world! one</h1>
+}
+export default elemento1;
